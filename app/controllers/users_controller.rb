a020d3796fc6dc75e2f4e9b2009360ffd21d9a43
cmonkey03 <russellpbrown@gmail.com>
2018-08-21 15:31:09 -0400

    Completed basic CRUD for Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,40 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
 
   def new
     @user = User.new
   end
 
   def create
-    @user = @user.create()
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to(@user)
+    else
+      render :new
+    end
   end
-  #
-  # def edit
-  # end
-  #
-  # def update
-  # end
-  #
-  # def destroy
-  # end
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    if @user.valid?
+      flash[:notice] = "You have successfully updated your profile."
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
 
   private
 
