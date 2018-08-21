8899d2dd1a88537662a82fbec82d09337e418673
github username <github email address>
2018-08-21 17:38:50 -0400

    Completed deliverables for venue and R for event

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,2 +1,16 @@
 class VenuesController < ApplicationController
+  before_action :set_venue, only: [:show]
+
+  def index
+    @venues = Venue.all
+  end
+
+  def show
+  end
+
+  private
+
+    def set_venue
+      @venue = Venue.find_by_id(params[:id])
+    end
 end
