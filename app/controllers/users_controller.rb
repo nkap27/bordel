56632c8b9f9c74130a7b57ad402e5fa806a809bb
cmonkey03 <russellpbrown@gmail.com>
2018-08-21 14:35:36 -0400

    Begin build of CR for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = @user.create()
+  end
+  #
+  # def edit
+  # end
+  #
+  # def update
+  # end
+  #
+  # def destroy
+  # end
+
+  private
+
+    def set_user
+      @user = User.find_by_id(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:username, :name, :muse, :bio)
+    end
+
 end
