5a573f4f00e46b9484dc7693fd1fedf817f749e2
cmonkey03 <russellpbrown@gmail.com>
2018-08-22 15:47:52 -0400

    MVP. Bitches. Minus login and a couple view deliverables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_22_163751) do
+ActiveRecord::Schema.define(version: 2018_08_22_192138) do
 
   create_table "comments", force: :cascade do |t|
     t.text "text"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2018_08_22_163751) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "venue_id"
+    t.integer "host_id"
     t.index ["venue_id"], name: "index_events_on_venue_id"
   end
 
