06d2e96e2e0a0598fa0210db55e8c11265008225
cmonkey03 <russellpbrown@gmail.com>
2018-08-24 11:12:45 -0400

    Added alternate logged_in? layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
-
   helper_method :current_user
   helper_method :logged_in?
   before_action :authorized
+
+  layout :layout_by_resource
+
+  def layout_by_resource
+    logged_in? ? "application" : "login"
+  end
+
 
   def current_user
     @user = User.find_by({ id: session[:user_id] })
